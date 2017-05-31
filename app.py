03064e082c993149a3d507fa795882fb932e7fea
skylite <skylite@air>
2017-05-31 12:41:24 +0200

    login

--- app.py
+++ app.py
@@ -1,46 +1,33 @@
 from flask import Flask
+from flask import request, redirect, url_for
 from flask import render_template
-from flask_login import LoginManager
-from flask_login import login_required
-
 
 app = Flask(__name__)
 app.debug = True
 
-login_manager = LoginManager()
-login_manager.session_protection = "strong"
-login_manager.init_app(app)
-
 # http://flask.pocoo.org
 # az index fugveny rendeleli le az index.html file-t
 
-class User:
-    def __init__(self, username):
-        self.username = username
-        self.loggedIn = False
-
-user1 = User("skylite")
-print(user1.username)
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'] )
 def index():
+    error = None
+    if request.method == 'POST':
+        email = request.form.get('email')
+        passwd = request.form.get('password')
+        print(email)
+        print(passwd)
+        if email == "skylite":
+            return redirect(url_for('nyilvantarto'))
+        else:
+            error = 'Invalid Credentials. Please try again.'
     # a flask minden template file-t a templates mappán belül keres.
-    return render_template('login.html')
-
+    return render_template('login.html', error=error)
 
 @app.route("/nyilvantarto")
-@login_required
-def loginPage():
+def nyilvantarto():
     user = "skylite"
     # a user változó, az az index.html file-ban is user változóként lesz elérhető
     return render_template('index.html', user=user)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    user = request.form.get('email')
-    user = User()
-    return flask.render_template('login.html', form=form)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
