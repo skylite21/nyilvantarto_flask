794fc9c667dda2f4bfa2ef7e207f490be9f4fb93
skylite21 <zsolt.lengyel21@gmail.com>
2017-06-02 11:24:08 +0200

    csinaltunk egy joszar logint

--- app.py
+++ app.py
@@ -7,26 +7,45 @@ app.debug = True
 
 # http://flask.pocoo.org
 # az index fugveny rendeleli le az index.html file-t
+# a / oldalon get és post method-okat is engedünk
+# https://www.w3schools.com/tags/ref_httpmethods.asp
+# EZ NEM EGY IGAZI BEJELENTKEZŐ FELÜLET, így nem szabad csinálni
+# rendes belejentkezést
+#
+validuser = "skylite"
 
-@app.route("/", methods=['GET', 'POST'] )
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    # egy error változót deklarálunk aminek a kezdő értéken None (azaz semmi)
     error = None
+    # ha ez a page ugy van meghivva hogy a kliens POST-ot kuld
     if request.method == 'POST':
+        # a login.html-ben lévő form adatokat, kiszedjük a POST-bol
+        # és betöltjük két darab változóba.
         email = request.form.get('email')
         passwd = request.form.get('password')
-        print(email)
-        print(passwd)
-        if email == "skylite":
+        print("A megadott emailcim: {}".format(email))
+        print("A megadott jelszo: {}".format(passwd))
+        # a beküldött adatok közül ellenőrizzük a felhasználónevet,
+        # és ha az helyes
+        if email == validuser:
+            # átirányítjuk a felhasználót a belső oldalra
             return redirect(url_for('nyilvantarto'))
         else:
+            # ha rossz adatokat adott meg, akkor egy hibaüzenetet küldünk..
             error = 'Invalid Credentials. Please try again.'
     # a flask minden template file-t a templates mappán belül keres.
+    # ha nem POST a http kérés, hanem GET, akkor simán csak a
+    # login.html-t rendereljük:
     return render_template('login.html', error=error)
+
 
 @app.route("/nyilvantarto")
 def nyilvantarto():
-    user = "skylite"
-    # a user változó, az az index.html file-ban is user változóként lesz elérhető
+    user = validuser
+    # a user változó, az az index.html file-ban is user változóként
+    # lesz elérhető
     return render_template('index.html', user=user)
 
 if __name__ == "__main__":
