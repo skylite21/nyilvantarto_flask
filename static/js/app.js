827b0ed3dba8f43047bc30f668affda7a3d0278b
skylite <skylite@air>
2017-06-08 13:32:36 +0200

    munkanap form+validacio

--- static/js/app.js
+++ static/js/app.js
@@ -18,18 +18,31 @@ function switchMenu(clickedId) {
   $("#"+clickedId+".menu-item").removeClass("btn-secondary");
   $("#"+clickedId+".menu-item").addClass("btn-primary");
 }
+function getToday() {
+	let now = new Date();
+  // let testDate = new Date("January 23, 2014 11:13:00");
+
+  // levágjuk az utolsó két elemet a strinből, és elé írunk egy 0 karaktert
+	let day = ("0" + now.getDate()).slice(-2);
+  // january starts with 0 ¯\(°_o)/¯ Ezért hozzá kell adnunk egyet
+	let month = ("0" + (now.getMonth() + 1)).slice(-2);
+	let today = now.getFullYear()+"."+(month)+"."+(day) ;
+	return today;
+}
+
+
 function addMunkanap() {
   let lastid = parseInt($('.new_munkanap:last').attr('id'));
-  console.log("lastid: "+lastid);
   if(isNaN(lastid)) {
     lastid=0; 
   }
+  mainap = getToday();
 
   $('#munkanapItems').append(`
         <div id="`+(lastid+1)+`" class="new_munkanap row">
           <button id="`+(lastid+1)+`" class="remove btn btn-danger" onclick="removeMunkanap(this.id)">-</button>
           <div class="col-xs-6 col-sm-2">
-            <input id="`+(lastid+1)+`" type="text" placeholder="dátum" class="datum_mezo form-control" >
+            <input id="`+(lastid+1)+`" type="text" placeholder="dátum" class="datepicker datum_mezo form-control" value="`+mainap+`" >
           </div>
           <div class="col-xs-6 col-sm-2">
             <input id="`+(lastid+1)+`" type="number" min="0.5" max="12" step="0.5" class="munkaora form-control" placeholder="munkaóra">
@@ -39,12 +52,38 @@ function addMunkanap() {
           </div>
         </div>
   `);
+  // bootstrap-datepicker modul:
+  // http://bootstrap-datepicker.readthedocs.io/en/latest/index.html
+  $('.datepicker').datepicker({
+      language: 'hu',
+      autoclose: 'true',
+      todayBtn: 'true',
+      todayHighlight: 'true'
+  });
+
+
 } // addMunkanap
+
+let new_munkanaps = [];
+// [{id:1, datePiced:"2017.04.03",workedHour:5,comment:"mycomment",okToSend:true},{},{}...]
+
+function collectMunkanaps() {
+  //TODO összegyűjteni a munkanapokat egy objecteket tartalmazó tömbbe
+  console.log("munkanapok összegyűjtése....");
+}
+
 
 function removeMunkanap(munkanapToRemove) {
   $("#"+munkanapToRemove+".new_munkanap").remove();
-  console.log("ezt kell torolni: " +munkanapToRemove);
 }
+
+
+function sendForm() {
+  console.log("sending form...");
+  collectMunkanaps();
+  //TODO elkuldeni az adatokat a szervernek
+}
+
 
 $(document).on('blur', '.munkaora' , function() {
   // amit ide irunk kod, az akkor fut le ha a munkaora mezöt elhagyja a user
@@ -52,8 +91,6 @@ $(document).on('blur', '.munkaora' , function() {
   // http://jsfiddle.net/4ksywa6d/1/
 
   munkaoraMezo = parseFloat($(this).val());
-  console.log("A munkaora mezo erteke: "+munkaoraMezo);
-  console.log("A munkaora mezo tipusa: "+typeof(munkaoraMezo));
   if ( Number.isFinite(munkaoraMezo) ) {
     if ( munkaoraMezo > 8 ) {
       $(this).val('8');
@@ -63,9 +100,6 @@ $(document).on('blur', '.munkaora' , function() {
   } else {
     $(this).val('0');
   }
-
-
-
 });
 
 $(document).ready(function () {
@@ -73,7 +107,7 @@ $(document).ready(function () {
   addMunkanap();
 });
 
-
+// firefox fix!! https://bugzilla.mozilla.org/show_bug.cgi?id=1012818
 $(function(){
   $("input[type='number']").on("click", function(){
     $(this).focus();
